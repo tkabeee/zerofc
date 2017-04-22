1868fdd61fc2d7ccb8aa7084d6ebcfe9a7cc4163
tkabeee <tkabee@gmail.com>
2017-04-22 15:01:40 +0900

    スタイル修正

--- zerofc.web.fc2.com/gulp/gulpfile.js
+++ zerofc.web.fc2.com/gulp/gulpfile.js
@@ -2,12 +2,12 @@
 
 var gulp = require("gulp");
 var webserver = require('gulp-webserver');
+var compass   = require("gulp-compass");
+var header    = require('gulp-header');
+var minifyCss = require("gulp-minify-css");
 var plumber   = require("gulp-plumber");
 var rename    = require('gulp-rename');
-var compass   = require("gulp-compass");
 var autoprefixer = require("gulp-autoprefixer");
-var concat    = require("gulp-concat");
-var minifyCss = require("gulp-minify-css");
 
 var APP_ROOT  = "../html";
 
@@ -18,7 +18,7 @@ var SASS_DIR  = "src/sass"
 var CSS_DIR   = "src/css"
 var CSS_DEST_DIR = APP_ROOT + "/css";
 
-gulp.task("compass", function(){
+gulp.task("compile", function(){
   gulp.src([SCSS_FILE])
     .pipe(plumber({
       errorHandler: function(error) {
@@ -30,20 +30,8 @@ gulp.task("compass", function(){
       sass: SASS_DIR,
       css: CSS_DIR
     }))
-    // .pipe(gulp.dest(CSS_DEST_DIR))
-    ;
-});
-
-gulp.task("concat", function(){
-  gulp.src([CSS_FILE])
-    .pipe(concat("set.css"))
-    .pipe(gulp.dest(CSS_DIR))
-    ;
-});
-
-gulp.task("minify", function(){
-  gulp.src([CSS_DIR + '/set.css'])
-    .pipe(plumber())
+    .pipe(header("@charset \"UTF-8\";\n\n"))
+    .pipe(gulp.dest(CSS_DEST_DIR))
     .pipe(minifyCss({
       'keepBreaks': false
     }))
@@ -53,13 +41,7 @@ gulp.task("minify", function(){
 });
 
 gulp.task("watch", function(){
-  gulp.watch(
-    [SCSS_FILE],
-    [
-      "compass",
-      "concat",
-      "minify"
-    ]
+  gulp.watch([SCSS_FILE],["compile"]
   );
 });
 
@@ -77,7 +59,5 @@ gulp.task("default", function(){
 });
 
 gulp.task("css", function(){
-  gulp.start('compass');
-  gulp.start('concat');
-  gulp.start('minify');
+  gulp.start('compile');
 });
