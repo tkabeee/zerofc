d94b1cf73b18efa1d8ef0ed9bd686fe844ab020c
tkabeee <tkabee@gmail.com>
2019-02-02 22:08:06 +0900

    地図が表示されてなかったので更新

--- zerofc.web.fc2.com/html/include/instructor.js
+++ zerofc.web.fc2.com/html/include/instructor.js
@@ -1,5 +1,5 @@
 document.write('\
-<div id="zero-instructor" style="margin-bottom:6px">\
+<div id="zero-instructor" style="margin-bottom:12px">\
   <a href="/instructor/" title="インストラクターの紹介" target="_self" style="display:block; padding:30px 0; background-color:#C20404; color:#fff; text-align:center;">\
     <span style="font-size:1.2em;font-weight:bold;">インストラクター紹介</span>\
   </a>\
---
+++ zerofc.web.fc2.com/html/include/zerodiary.js
@@ -0,0 +1,7 @@
+document.write('\
+<div class="zero-diary" style="margin-bottom:12px">\
+  <a href="http://zfcj.blog98.fc2.com/" title="『零日記』岡山のゼロ戦クラブの日常をつづっているブログ" target="_blank" style="display:block; padding:30px 0; background-color:#555555; color:#fff; text-align:center;">\
+    <span style="font-size:1.2em;font-weight:bold;">零日記</span>\
+  </a>\
+</div>\
+');
