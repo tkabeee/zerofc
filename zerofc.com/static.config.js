87c5211b5c2dd00ed15ca5c86966930f4ff84ac1
tkabeee <tkabee@gmail.com>
2019-04-30 22:08:01 +0900

    update config

--- zerofc.com/static.config.js
+++ zerofc.com/static.config.js
@@ -1,7 +1,12 @@
 import path from 'path'
 import axios from 'axios'
+import React from 'react'
 
 export default {
+  siteRoot: 'https://zerofc.web.fc2.com/',
+  getSiteData: async ({ dev }) => ({
+    title: '岡山総合格闘技ジム ゼロ戦クラブ',
+  }),
   getRoutes: async () => {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
@@ -23,6 +28,22 @@ export default {
       },
     ]
   },
+  Document: ({
+    Html,
+    Head,
+    Body,
+    children,
+    state: { siteData, renderMeta },
+  }) => (
+    <Html lang="ja">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="VY6apQgrtzLTC_yR2i9QmKyTyc4xV9Injys1efZocMU" />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
   plugins: [
     [
       require.resolve('react-static-plugin-source-filesystem'),
