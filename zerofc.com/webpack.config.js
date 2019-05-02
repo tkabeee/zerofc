142ead3067ae839b39cd1ef5d5a7500af1cdb9cd
tkabeee <tkabee@gmail.com>
2019-05-02 20:06:40 +0900

    add webpack config only sass build

---
+++ zerofc.com/webpack.config.js
@@ -0,0 +1,31 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  context: path.join(__dirname, './src/styles'),
+  entry: {
+    style: './style.scss',
+  },
+  output: {
+    path: __dirname + '/public/css',
+    filename: '[name].css'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+}
