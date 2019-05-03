bd5a2ebaa8814b3e46148023996e33ceecd9081c
tkabeee <tkabee@gmail.com>
2019-05-03 22:24:05 +0900

    add main image

--- zerofc.com/src/pages/index.js
+++ zerofc.com/src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default () => (
-  <div className="main__">
-   <div className="">
-     <span>打撃・グラップリング・柔術・レスリング・キックボクシングなど各自の希望に合わせた練習を行っています。初心者や未経験の方でも楽しく練習ができる格闘技ジムです。日本修斗協会公認オフィシャルジムとして岡山・倉敷・水島・総社で活動中です。</span>
-   </div>
+  <div>
+    <div className="main__image">
+      <div className="inside-text">打撃・グラップリング・柔術・レスリング・キックボクシングなど各自の希望に合わせた練習を行っています。初心者や未経験の方でも楽しく練習ができる格闘技ジムです。日本修斗協会公認オフィシャルジムとして岡山・倉敷・水島・総社で活動中です。</div>
+    </div>
     <iframe className="main__frame" src="/frames/root/top2.html" scrolling="no" frameborder="0">この部分はインラインフレームを使用しています。</iframe>
   </div>
 )
