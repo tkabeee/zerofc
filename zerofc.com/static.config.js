bdd1b74671a9bd03aa45037670639370e5caf889
tkabeee <tkabee@gmail.com>
2019-05-03 15:32:17 +0900

    set sass config

--- zerofc.com/static.config.js
+++ zerofc.com/static.config.js
@@ -51,6 +51,7 @@ export default {
         location: path.resolve('./src/pages'),
       },
     ],
+    require.resolve('react-static-plugin-sass'),
     require.resolve('react-static-plugin-reach-router'),
     require.resolve('react-static-plugin-sitemap'),
   ],
