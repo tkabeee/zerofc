c3eed862f7d745aef7016cf33ced4b016ca90fcc
tkabeee <tkabee@gmail.com>
2019-05-03 15:32:24 +0900

    update header

--- zerofc.com/src/components/header/index.js
+++ zerofc.com/src/components/header/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Link } from 'components/Router'
 
 import './style.scss'
 
 export default () => (
-  <header class="header-container">
-    <h1 id="logo"><a href="/" title=""><span>岡山総合格闘技ジム&nbsp;ゼロ戦クラブ</span></a></h1>
-    <ul class="header-sub-menu clearfix">
-      <li class="menu-contact"><a href="mailto:&#122;&#102;&#99;&#100;&#111;&#110;&#103;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;" title="お問い合わせ"><span>お問い合わせ</span></a></li>
-      <li class="menu-faq"><a href="/faq/" title="よくあるご質問" target="_self"><span>よくあるご質問</span></a></li>
-      <li class="menu-sitemap"><a href="/sitemap/" title="サイトマップ" target="_self"><span>サイトマップ</span></a></li>
+  <header class="global-header">
+    <div className="header-main">
+      <h1 className="site-title">
+        <Link to="/" title="TOP"><img className="header__logo" src="/images/logo.jpg" />岡山総合格闘技ジム ゼロ戦クラブ</Link>
+      </h1>
+    </div>
+    <ul class="header__menu-list">
+      <li class="list-item"><a href="mailto:&#122;&#102;&#99;&#100;&#111;&#110;&#103;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;" title="お問い合わせ"><span>お問い合わせ</span></a></li>
+      <li class="list-item"><Link to="/faq/" title="よくあるご質問">よくあるご質問</Link></li>
+      <li class="list-item"><Link to="/sitemap/" title="サイトマップ">サイトマップ</Link></li>
     </ul>
   </header>
 )
