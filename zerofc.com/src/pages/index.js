4173f521a2e60a466d25fb0143ed514c15268010
tkabeee <tkabee@gmail.com>
2019-05-06 17:11:32 +0900

    invalid dom property

--- zerofc.com/src/pages/index.js
+++ zerofc.com/src/pages/index.js
@@ -5,6 +5,6 @@ export default () => (
     <div className="main__image">
       <div className="inside-text">打撃・グラップリング・柔術・レスリング・キックボクシングなど各自の希望に合わせた練習を行っています。初心者や未経験の方でも楽しく練習ができる格闘技ジムです。日本修斗協会公認オフィシャルジムとして岡山・倉敷・水島・総社で活動中です。</div>
     </div>
-    <iframe className="main__frame" src="/frames/root/top2.html" scrolling="no" frameborder="0">この部分はインラインフレームを使用しています。</iframe>
+    <iframe className="main__frame" src="/frames/root/top2.html" scrolling="no" frameBorder="0">この部分はインラインフレームを使用しています。</iframe>
   </div>
 )
