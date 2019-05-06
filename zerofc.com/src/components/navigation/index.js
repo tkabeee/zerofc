13763848c5728b9636a6f6a89b8a6870d58ea338
tkabeee <tkabee@gmail.com>
2019-05-06 22:50:40 +0900

    improve link

--- zerofc.com/src/components/navigation/index.js
+++ zerofc.com/src/components/navigation/index.js
@@ -12,7 +12,7 @@ export default () => (
         <li className="nav-item nav-item-guide"><Link to="/guide/" title="施設案内">施設案内</Link></li>
         <li className="nav-item nav-item-schedule"><Link to="/schedule/" title="練習スケジュール">練習スケジュール</Link></li>
         <li className="nav-item nav-item-kids"><Link to="/kids/" title="キッズ">キッズ</Link></li>
-        <li className="nav-item nav-item-video"><Link to="http://www.youtube.com/user/zerofighterclub?feature=mhee" title="動画" target="_blank">動 画</Link></li>
+        <li className="nav-item nav-item-video"><a href="http://www.youtube.com/user/zerofighterclub?feature=mhee" title="動画" target="_blank">動 画</a></li>
         <li className="nav-item nav-item-access"><Link to="/access/" title="アクセスマップ">アクセスマップ</Link></li>
         <li className="nav-item nav-item-link"><Link to="/link/" title="リンク">リンク</Link></li>
       </ul>
