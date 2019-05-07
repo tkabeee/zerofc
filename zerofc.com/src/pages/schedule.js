b3f427d34a59173d8bcc81f2b67bc88294b95f41
tkabeee <tkabee@gmail.com>
2019-05-07 23:13:23 +0900

    add content for schedule

--- zerofc.com/src/pages/schedule.js
+++ zerofc.com/src/pages/schedule.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { useRouteData } from 'react-static'
+import Schedule from '../contents/schedule'
 
-export default () => {
-  const data = useRouteData()
-  return (
-    <div className="dangerously" dangerouslySetInnerHTML={{__html: data.schedule}}></div>
-  )
-}
+export default () => (
+  <Schedule />
+)
