e7406aa6cbfbd09c0928e11103de05ae7611e8e5
tkabeee <tkabee@gmail.com>
2019-05-07 23:02:30 +0900

    add contents

--- zerofc.com/src/pages/guide.js
+++ zerofc.com/src/pages/guide.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { useRouteData } from 'react-static'
+import Guide from '../contents/guide'
 
-export default () => {
-  const data = useRouteData()
-  return (
-    <div className="dangerously" dangerouslySetInnerHTML={{__html: data.guide}}></div>
-  )
-}
+export default () => (
+  <Guide />
+)
