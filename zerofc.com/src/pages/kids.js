18a0d91247e266a5f1e7ebac2adc354d884fb530
tkabeee <tkabee@gmail.com>
2019-05-07 23:17:49 +0900

    add content for kids

--- zerofc.com/src/pages/kids.js
+++ zerofc.com/src/pages/kids.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { useRouteData } from 'react-static'
+import Kids from '../contents/kids'
 
-export default () => {
-  const data = useRouteData()
-  return (
-    <div className="dangerously" dangerouslySetInnerHTML={{__html: data.kids}}></div>
-  )
-}
+export default () => (
+  <Kids />
+)
