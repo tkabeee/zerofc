caa91297026674708c904065a0657cdb4913d4fc
tkabeee <tkabee@gmail.com>
2019-05-07 22:53:11 +0900

    refactoring

--- zerofc.com/src/pages/admission.js
+++ zerofc.com/src/pages/admission.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { useRouteData } from 'react-static'
+import Admission from '../contents/admission'
 
-export default () => {
-  const data = useRouteData()
-  return (
-    <div className="dangerously" dangerouslySetInnerHTML={{__html: data.admission}}></div>
-  )
-}
+export default () => (
+  <Admission />
+)
