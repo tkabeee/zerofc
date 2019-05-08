6edede2e76fae86880fe98d0147b62bea0261c08
tkabeee <tkabee@gmail.com>
2019-05-09 00:12:07 +0900

    refactoring

--- zerofc.com/src/components/widgets/fb-page.js
+++ zerofc.com/src/components/widgets/fb-page.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
-document.write('<div id="fb-root"></div>');
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.9";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-export default () => (
-  <div className="zero-facebook">
-    <div className="fb-page" data-href="https://www.facebook.com/&#x30bc;&#x30ed;&#x6226;&#x30af;&#x30e9;&#x30d6;-294718787233613" data-height="800" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-      <blockquote cite="https://www.facebook.com/&#x30bc;&#x30ed;&#x6226;&#x30af;&#x30e9;&#x30d6;-294718787233613" className="fb-xfbml-parse-ignore">
-      <a href="https://www.facebook.com/&#x30bc;&#x30ed;&#x6226;&#x30af;&#x30e9;&#x30d6;-294718787233613">ゼロ戦クラブ Facebookページ</a>
-      </blockquote>
+export default () => {
+  return (
+    <div className="zero-facebook">
+      <div className="fb-page" data-href="https://www.facebook.com/&#x30bc;&#x30ed;&#x6226;&#x30af;&#x30e9;&#x30d6;-294718787233613" data-height="800" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+        <blockquote cite="https://www.facebook.com/&#x30bc;&#x30ed;&#x6226;&#x30af;&#x30e9;&#x30d6;-294718787233613" className="fb-xfbml-parse-ignore">
+          <a href="https://www.facebook.com/&#x30bc;&#x30ed;&#x6226;&#x30af;&#x30e9;&#x30d6;-294718787233613">ゼロ戦クラブ Facebookページ</a>
+        </blockquote>
+      </div>
     </div>
-  </div>
-)
+  )
+}
+
+if (typeof document !== 'undefined') {
+  document.write('<div id="fb-root"></div>')
+  ;(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0]
+    if (d.getElementById(id)) return
+    js = d.createElement(s); js.id = id
+    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.9"
+    fjs.parentNode.insertBefore(js, fjs)
+  }(document, 'script', 'facebook-jssdk'))
+}
