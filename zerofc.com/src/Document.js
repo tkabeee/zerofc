5ab79342b23f7ce1d5474190c384efe58066c9fc
tkabeee <tkabee@gmail.com>
2019-05-14 23:03:49 +0900

    improve

--- zerofc.com/src/Document.js
+++ zerofc.com/src/Document.js
@@ -28,7 +28,25 @@ export default class CustomHtml extends Component {
             }}
           />
         </Head>
-        <Body>{children}</Body>
+        <Body>
+          {children}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (typeof document !== 'undefined') {
+                  document.write('<div id="fb-root"></div>');
+                  (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.9";
+                    fjs.parentNode.insertBefore(js, fjs);
+                  }(document, 'script', 'facebook-jssdk'));
+                }
+              `,
+            }}
+          />
+        </Body>
       </Html>
     )
   }
--- zerofc.com/src/components/widgets/fb-page.js
+++ zerofc.com/src/components/widgets/fb-page.js
@@ -11,14 +11,3 @@ export default () => {
     </div>
   )
 }
-
-if (typeof document !== 'undefined') {
-  document.write('<div id="fb-root"></div>')
-  ;(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0]
-    if (d.getElementById(id)) return
-    js = d.createElement(s); js.id = id
-    js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.9"
-    fjs.parentNode.insertBefore(js, fjs)
-  }(document, 'script', 'facebook-jssdk'))
-}
