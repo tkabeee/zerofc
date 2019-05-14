0c9f281d25b1bd2c734c5c2e4f1ae382d468cedb
tkabeee <tkabee@gmail.com>
2019-05-14 23:55:09 +0900

    update head

--- zerofc.com/src/components/Head.js
+++ zerofc.com/src/components/Head.js
@@ -9,10 +9,13 @@ export default ({
 }) => {
   return (
     <Head>
-      {children}
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="google-site-verification" content="VY6apQgrtzLTC_yR2i9QmKyTyc4xV9Injys1efZocMU" />
       {title && <title>{title}</title>}
       {description && <meta itemProp="description" content={description} />}
       {keywords && <meta itemProp="keywords" content={keywords.join(',')} />}
+      {children}
     </Head>
   )
 }
